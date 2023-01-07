29db5f5b337f9f250500c644d2f85a6cf4c2dcd3
Yongheng Chen <changochen1@gmail.com>
2023-01-07 00:36:29 +0000

    Improve crash triage

--- srcs/crash_triage.py
+++ srcs/crash_triage.py
@@ -4,8 +4,21 @@ Categorize the crashes.
 
 import fire
 import subprocess
+import re
 
 from utils import afl
+
+
+class ErrorInfo(object):
+
+  def __init__(self, error_type=None, error_loc=None, error_msg=None, bt=None):
+    self.error_type = error_type
+    self.error_loc = error_loc
+    self.error_msg = error_msg
+    self.bt = bt
+
+  def __str__(self):
+    return f"Error type: {self.error_type}, loc: {self.error_loc}, msg: {self.error_msg}"
 
 
 def run_target_and_get_error(cmd):
@@ -14,11 +27,35 @@ def run_target_and_get_error(cmd):
   return std_err.decode("utf-8")
 
 
-def get_assertion_msg(msg):
+def extract_asan_error(msg):
+  result = ErrorInfo()
+  asan_type_pattern = r"SUMMARY: AddressSanitizer: ([^\s]+) ([^\s]+) in (.+)"
   for line in msg.split("\n"):
-    if "Assertion" in line or "Seg" in line or "==ERROR" in line:
-      return line
-  return ""
+    if "SUMMARY" in line:
+      match = re.search(asan_type_pattern, line)
+      assert match
+      result.error_type = match.group(1)
+      result.error_loc = match.group(3)
+      result.error_msg = match.group(2)
+      return result
+  assert False
+
+
+def get_err_info(msg):
+  #assertion_pattern = "(Assertion .* failed)"
+  if "==ERROR: AddressSanitizer" in msg:
+    return extract_asan_error(msg)
+  for line in msg.split("\n"):
+    if "Assertion" in line and "failed" in line:
+      component = line.split(": ")
+      result = ErrorInfo()
+      result.error_type = "Assertion failure"
+      result.error_loc = component[3]
+      result.error_msg = component[2]
+      return result
+    if "Seg" in line or "==ERROR" in line:
+      assert False
+  return None
 
 
 def triage(cmd, path):
@@ -29,12 +66,18 @@ def triage(cmd, path):
       tmp_cmd = cmd.replace("@@", repr(crash))
     else:
       tmp_cmd = f"{cmd} <{repr(crash)}"
-    msg = get_assertion_msg(run_target_and_get_error(tmp_cmd))
+    msg = get_err_info(run_target_and_get_error(tmp_cmd))
     if not msg:
       continue
-    result[msg] = crash
+    if msg.error_type not in result:
+      result[msg.error_type] = {}
+    result[msg.error_type][msg.error_loc] = crash
   for key, value in result.items():
-    print(key, value)
+    print(f"Error type: {key}")
+    for idx, (error_loc, file_path) in enumerate(value.items()):
+      print("+" * 40)
+      print(f"{idx}: {error_loc} at {file_path}")
+    print("=" * 40)
 
 
 if __name__ == "__main__":
